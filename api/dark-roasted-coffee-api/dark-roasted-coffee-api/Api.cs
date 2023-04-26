c9514d758747d465bf16b7607daf4a4cf5e5fe74
punkt <punkthor@gmail.com>
2023-04-26 23:31:18 +0200

    Removed commented-out code

--- api/dark-roasted-coffee-api/dark-roasted-coffee-api/Api.cs
+++ api/dark-roasted-coffee-api/dark-roasted-coffee-api/Api.cs
@@ -6,7 +6,7 @@ namespace dark_roasted_coffee_api
 {
     public static class Api
     {
-        // Endpoint mapping
+        // Endpoint mapping including swagger descriptions/annotations
         public static void ConfigureApi(this WebApplication app)
         {
             app.MapGet("/coffeedrinks", GetAll)
@@ -30,37 +30,6 @@ namespace dark_roasted_coffee_api
                     Summary = "Retrieve a Coffee Drink",
                     Description = "Retrieves a single `Coffee Drink` object. It has details about a particular coffee drink."
                 });
-            // Get all items
-            /*app.MapGet("/coffeedrinks", async (IGetData data) =>
-                await data.GetAllAsync()
-                is IEnumerable<CoffeeDrink> coffeeDrinks
-                ? TypedResults.Ok(coffeeDrinks)
-                : Results.NotFound())
-                .Produces<List<CoffeeDrink>>(StatusCodes.Status200OK)
-                .Produces(StatusCodes.Status404NotFound)
-                .WithName("listCoffeeDrinks")
-                .WithTags("Coffee Drinks")
-                .WithOpenApi(operation => new(operation)
-                {
-                    Summary = "List Coffee Drinks",
-                    Description = "Returns a list of `Coffe Drink` objects."
-                });*/
-
-            // Get one item by Id
-            /*app.MapGet("/coffeedrinks{id}", async (IGetData data, int id) =>
-            {
-                var result = await data.GetOneAsync(id);
-                return result.Any() ? Results.Ok(result) : Results.NotFound();
-            })
-            .Produces<IEnumerable<CoffeeDrink>>(StatusCodes.Status200OK)
-            .Produces(StatusCodes.Status404NotFound)
-            .WithTags("Coffee Drinks")
-            .WithName("retrieveCoffeeDrink")
-            .WithOpenApi(operation => new(operation)
-            {
-                Summary = "Retrieve a Coffee Drink",
-                Description = "Retrieves a single `Coffee Drink` object. It has details about a particular coffee drink."
-            });*/
 
             static async Task<IResult> GetAll (IGetData data)
             {
